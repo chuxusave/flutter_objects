403abdd8e4e2d436b31a4a30b6cfa2f76258f67e
chuxu <chuxu@chuxudeMacBook-Pro-2.local>
2020-04-30 15:34:30 +0800

    组合和自绘

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -15,158 +16,56 @@ class MyApp extends StatelessWidget {
       );
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        body: TabBarView(
-          children: [
-            ParallelWidget(),
-            ScrollNotificationWidget(),
-            ScrollControllerWidget(),
-          ],
-        ),
-        bottomNavigationBar: TabBar(
-          tabs: [
-            Tab(
-              icon: Icon(Icons.home),
-              text: "视差",
-            ),
-            Tab(
-              icon: Icon(Icons.rss_feed),
-              text: "Notification",
-            ),
-            Tab(
-              icon: Icon(Icons.perm_identity),
-              text: "Controller",
-            )
-          ],
-          unselectedLabelColor: Colors.blueGrey,
-          labelColor: Colors.blue,
-          indicatorSize: TabBarIndicatorSize.label,
-          indicatorColor: Colors.red,
-        ),
-      ),
-    );
-  }
+  State<StatefulWidget> createState() => _MyHomePageState();
 }
 
-class ParallelWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(slivers: <Widget>[
-      SliverAppBar(
-        //SliverAppBar 作为头图控件
-        title: Text('CustomScrollView Demo'), // 标题
-        floating: true, // 设置悬浮样式
-        flexibleSpace: Image.network(
-            "https://media-cdn.tripadvisor.com/media/photo-s/13/98/8f/c2/great-wall-hiking-tours.jpg",
-            fit: BoxFit.cover), // 设置悬浮头图背景
-        expandedHeight: 280, // 头图控件高度
-      ),
-      SliverList(
-        //SliverList 作为列表控件
-        delegate: SliverChildBuilderDelegate(
-          (context, index) => ListTile(title: Text('Item #$index')), // 列表项创建方法
-          childCount: 100, // 列表元素个数
-        ),
-      ),
-    ]);
-  }
-}
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  //使用控制Tabbar切换
+  TabController _tabController;
 
-class ScrollNotificationWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Scroll Notification Demo',
-        home: Scaffold(
-            appBar: AppBar(title: Text('ScrollController Demo')),
-            body: NotificationListener<ScrollNotification>(
-              // 添加 NotificationListener 作为父容器
-              onNotification: (scrollNotification) {
-                // 注册通知回调
-                if (scrollNotification is ScrollStartNotification) {
-                  // 滚动开始
-                  print('Scroll Start');
-                } else if (scrollNotification is ScrollUpdateNotification) {
-                  // 滚动位置更新
-                  print('Scroll Update');
-                } else if (scrollNotification is ScrollEndNotification) {
-                  // 滚动结束
-                  print('Scroll End');
-                }
-              },
-              child: ListView.builder(
-                itemCount: 30, // 列表元素个数
-                itemBuilder: (context, index) =>
-                    ListTile(title: Text("Index : $index")), // 列表项创建方法
-              ),
-            )));
-  }
-}
-
-class ScrollControllerWidget extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _ScrollControllerState();
-}
-
-class _ScrollControllerState extends State<ScrollControllerWidget> {
-  ScrollController _controller; //ListView 控制器
-  bool isToTop = false; // 标示目前是否需要启用 "Top" 按钮
-  @override
   void initState() {
-    _controller = ScrollController();
-    _controller.addListener(() {
-      // 为控制器注册滚动监听方法
-      if (_controller.offset > 1000) {
-        // 如果 ListView 已经向下滚动了 1000，则启用 Top 按钮
-        setState(() {
-          isToTop = true;
-        });
-      } else if (_controller.offset < 300) {
-        // 如果 ListView 向下滚动距离不足 300，则禁用 Top 按钮
-        setState(() {
-          isToTop = false;
-        });
-      }
-    });
     super.initState();
+    _tabController = new TabController(vsync: this, length: 2);
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Scroll Controller Widget")),
-      body: Column(
+      appBar: AppBar(
+        title: Text(widget.title),
+        bottom: TabBar(
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.system_update), text: "组合"),
+            Tab(icon: Icon(Icons.cake), text: "自绘")
+          ],
+          controller: _tabController,
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
         children: <Widget>[
-          Container(
-            height: 40.0,
-            child: RaisedButton(
-              onPressed: (isToTop
-                  ? () {
-                      if (isToTop) {
-                        _controller.animateTo(.0,
-                            duration: Duration(milliseconds: 200),
-                            curve: Curves.ease); // 做一个滚动到顶部的动画
-                      }
-                    }
-                  : null),
-              child: Text("Top"),
-            ),
+          ListView(
+            children: <Widget>[
+              UpdatedItemWidget(
+                model: UpdatedItemModel(
+                    appIcon: "assets/icon.png",
+                    appDescription:
+                        "Thanks for using Google Maps! This release brings bug fixes that improve our product to help you discover new places and navigate to them.",
+                    appName: "Google Maps - Transit & Fond",
+                    appSize: "137.2",
+                    appVersion: "Version 5.19",
+                    appDate: "2019年6月5日"),
+                onPressed: () {},
+              )
+            ],
           ),
-          Expanded(
-            child: ListView.builder(
-              controller: _controller, // 初始化传入控制器
-              itemCount: 100, // 列表元素总数
-              itemBuilder: (context, index) =>
-                  ListTile(title: Text("Index : $index")), // 列表项构造方法
-            ),
-          ),
+          Center(child: Cake())
         ],
       ),
     );
@@ -174,7 +73,143 @@ class _ScrollControllerState extends State<ScrollControllerWidget> {
 
   @override
   void dispose() {
-    _controller.dispose(); // 销毁控制器
+    _tabController.dispose();
     super.dispose();
   }
+}
+
+class UpdatedItemModel {
+  String appIcon;
+  String appName;
+  String appSize;
+  String appDate;
+  String appDescription;
+  String appVersion;
+  UpdatedItemModel(
+      {this.appIcon,
+      this.appName,
+      this.appSize,
+      this.appDate,
+      this.appDescription,
+      this.appVersion});
+}
+
+class UpdatedItemWidget extends StatelessWidget {
+  final UpdatedItemModel model;
+  UpdatedItemWidget({Key key, this.model, this.onPressed}) : super(key: key);
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    //组合上下两部分
+    return Column(
+        children: <Widget>[buildTopRow(context), buildBottomRow(context)]);
+  }
+
+//创建上半部分
+  Widget buildTopRow(BuildContext context) {
+    return Row(children: <Widget>[
+      Padding(
+          padding: EdgeInsets.all(10),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset(model.appIcon, width: 80, height: 80))),
+      Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              model.appName,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 20, color: Color(0xFF8E8D92)),
+            ),
+            Text(
+              "${model.appDate}",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 16, color: Color(0xFF8E8D92)),
+            ),
+          ],
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+        child: FlatButton(
+          color: Color(0xFFF1F0F7),
+          highlightColor: Colors.blue[700],
+          colorBrightness: Brightness.dark,
+          child: Text(
+            "OPEN",
+            style: TextStyle(
+                color: Color(0xFF007AFE), fontWeight: FontWeight.bold),
+          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          onPressed: onPressed,
+        ),
+      )
+    ]);
+  }
+
+  //创建下半部分
+  Widget buildBottomRow(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(model.appDescription),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Text("${model.appVersion} • ${model.appSize} MB"))
+            ]));
+  }
+}
+
+class Cake extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: Size(200, 200),
+      painter: WheelPainter(),
+    );
+  }
+}
+
+class WheelPainter extends CustomPainter {
+  //设置画笔颜色
+  Paint getColoredPaint(Color color) {
+    Paint paint = Paint();
+    paint.color = color;
+    return paint;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    //饼图尺寸
+    double wheelSize = min(size.width, size.height) / 2;
+    double nbElem = 6;
+    double radius = (2 * pi) / nbElem;
+    Rect boundingRect = Rect.fromCircle(
+        center: Offset(wheelSize, wheelSize), radius: wheelSize);
+
+    //画圆弧，每次1/6个圆弧
+    canvas.drawArc(
+        boundingRect, 0, radius, true, getColoredPaint(Colors.orange));
+    canvas.drawArc(
+        boundingRect, radius, radius, true, getColoredPaint(Colors.black38));
+    canvas.drawArc(
+        boundingRect, radius * 2, radius, true, getColoredPaint(Colors.green));
+    canvas.drawArc(
+        boundingRect, radius * 3, radius, true, getColoredPaint(Colors.red));
+    canvas.drawArc(
+        boundingRect, radius * 4, radius, true, getColoredPaint(Colors.blue));
+    canvas.drawArc(
+        boundingRect, radius * 5, radius, true, getColoredPaint(Colors.pink));
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => oldDelegate != this;
 }
